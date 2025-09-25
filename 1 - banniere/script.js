5a69e1b57b579082128a2d9b3cf78d0df8019dd3
Javier <Javier@gmail.com>
2025-09-25 13:43:44 +0200

    ejercicio banner completado

--- 1 - banniere/script.js
+++ 1 - banniere/script.js
@@ -6,6 +6,18 @@ Nouvelles compétences:
 
 // 1. Je recupere le bouton "Accepter les cookies"
 
+const btn_cookies = document.querySelector(".btn-accept");
+//console.log(btn_cookies);
+
 // 2. Je recupere la banniere de cookies
 
+const banniere = document.querySelector(".cookies");
+
 // 3. J'ecoute l'evenement "click" sur le bouton "Accepter les cookies"
+
+btn_cookies.addEventListener("click", function(event_obj){
+
+    banniere.style.opacity = 0;
+
+    
+})
