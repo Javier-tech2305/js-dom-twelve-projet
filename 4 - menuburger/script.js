da380fa760b1141b5a7a1b4561046392bba5875b
Javier <Javier@gmail.com>
2025-09-26 14:18:14 +0200

    dark menuburguer completo

--- 4 - menuburger/script.js
+++ 4 - menuburger/script.js
@@ -3,3 +3,15 @@
 const burger = document.querySelector(".burger");
 // 2. Je recupere la fenetre modal
 const modal = document.querySelector(".modal");
+//escucha el click en el burger
+burger.addEventListener("click",
+    function(){
+        //cambia la clase a show modal 
+
+        modal.classList.toggle("show-modal")
+        // cambia la clase  gurger por fa-x 
+        burger.classList.toggle("fa-x")
+
+     
+    }
+)
