40445fc96b20515ff3d263a0a01e7a4aff68d68d
Javier <Javier@gmail.com>
2025-09-26 09:35:32 +0200

    dark mode completo

--- 3 - dark mode/script.js
+++ 3 - dark mode/script.js
@@ -3,3 +3,11 @@ Nouvelles compétences:
 - forEach
 - querySelectorAll
 */
+
+const btn_dark = document.querySelector(".btn");
+btn_dark.addEventListener("click", function(){
+
+    const all_elem = document.querySelectorAll("*");
+    all_elem.forEach(all_elem =>
+    all_elem.classList.toggle("darkmode") );
+})
